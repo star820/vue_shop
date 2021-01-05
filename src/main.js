554c379cec46480b60c411c446f7d3cc4ea20ca1
e <1689373546@qq.com>
2021-01-05 19:15:51 +0800

    完成登录功能3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+// 导入字体图标
+import '../src/assets/fonts/iconfont.css'
 import './assets/css/global.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
