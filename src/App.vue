65d9615e2b7107016a065879e1d59367282c41a6
e <1689373546@qq.com>
2021-01-06 16:59:32 +0800

    用户列表页面绘制完成

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <!-- 路由占位符 -->
     <router-view></router-view>
-    app组件
   </div>
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -21,6 +21,8 @@
           unique-opened
           :collapse="isCollapse"
           :collapse-transition="false"
+          router
+          :default-active="activePath"
         >
           <!-- 一级菜单 -->
           <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
@@ -33,7 +35,7 @@
             </template>
 
             <!-- 二级菜单 -->
-            <el-menu-item :index="subItem.id + '' " v-for="subItem in item.children" :key="subItem.id">
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-menu"></i>
@@ -65,11 +67,15 @@ export default {
         102: 'iconfont icon-danju',
         145: 'iconfont icon-baobiao'
       },
-      isCollapse: false
+      // 是否折叠
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath: ''
     }
   },
   created() {
     this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
   },
   methods: {
     logout() {
@@ -85,6 +91,11 @@ export default {
     // 点击后让菜单折叠展开
     toggleCollapse() {
       this.isCollapse = !this.isCollapse
+    },
+    // 保存链接的激活状态
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     }
   }
 }
